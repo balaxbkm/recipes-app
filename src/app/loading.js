30d6771cebd1ebf8a3733edaf742921a0e98f2a8
Your Name <you@example.com>
2024-10-21 16:19:54 +0530

    Loading Page Improved

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,10 +1,14 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import { LoaderPinwheelIcon } from "lucide-react";
 
 export default function Loading() {
     return (
-        <div className="w-full min-h-screen">
-            Loading...
-            <Skeleton />
-        </div>
+        <>
+            <div className="w-full h-1 bg-red-200 absolute top-0"></div>
+            <div className="w-full min-h-screen flex items-center justify-center">
+                <div className="bg-red-100 drop-shadow-sm rounded-full p-0.5">
+                    <LoaderPinwheelIcon className="animate-spin h-8 w-8 text-red-400" />
+                </div>
+            </div>
+        </>
     );
 }
